6f4a513af56b8324c0cd1ec58cc665cdeecb46c2
Josh Harrington <josh.harrington@readingroom.com>
2015-04-09 14:31:44 +0100

    Add Starter Kit framework and bits of content

---
+++ content/introText.php
@@ -0,0 +1,11 @@
+<section class="intro">
+    <h3>We're doing a hackathon.</h3>
+    <p><strong>What?</strong></p>
+    <p><a href="http://openhack.github.io/">http://openhack.github.io/</a></p>
+    <p><strong>Why?</strong></p>
+    <p>To have some fun, come up with interesting ideas, start discussions.</p>
+    <p><strong>When?</strong></p>
+    <p>May 7th 2015</p>
+    <p><strong>Food?</strong></p>
+    <p>Yup, we're ordering a vast amount of pizza for y'all.</p>
+</section>
---
+++ content/titleBlock.php
@@ -0,0 +1,7 @@
+<section class="titleBlock">
+   <img src="/img/lightBulb.jpg" alt="">
+    <h1>RAPIDHACK</h1>
+    
+    
+</section>
+
